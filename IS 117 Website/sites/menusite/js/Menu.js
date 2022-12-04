3590ae6910b74594894c610b6397497ba92cbbaf
jgb36 <115026035+jgb36@users.noreply.github.com>
2022-12-04 15:29:22 -0500

    Delete IS 117 Website directory

--- IS 117 Website/sites/menusite/js/Menu.js
+++
@@ -1,7 +0,0 @@
-function init(){
-    document.getElementById("createForm").addEventListener("submit", checkForEmptyFields);
-}
-window.addEventListener("load", init);
-const dishCode = document.getElementById("Dish Code");
-const dishName = document.getElementById("Dish Name");
-const price = document.getElementById("Price");
